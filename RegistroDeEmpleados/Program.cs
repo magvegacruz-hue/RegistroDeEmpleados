bd1c5c8ff803abef3936a7b54d60ebe9eb48e18e
miguel <magvegacruz@uamv.edu.ni>
2026-06-01 12:26:12 -0600

    Agregue cosas

--- RegistroDeEmpleados/Program.cs
+++ RegistroDeEmpleados/Program.cs
@@ -1,6 +1,8 @@
 ﻿Empleado[] empleados = new Empleado[10];
+Console.WriteLine("Sea bienvenido al registro de empleado");
 void agregarEmpleado(int pos)
 {
+    Console.WriteLine("===================================");
     Console.Write("Nombre: ");
     empleados[pos].nombres = Console.ReadLine()!;
     Console.Write("Apellido: ");
@@ -12,27 +14,64 @@ void agregarEmpleado(int pos)
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Registro guardado satisfactoriamente");
     Console.ResetColor();
+    Console.WriteLine("===================================");
+}
+void guardarEmpleados(int pos)
+{
+    StreamWriter archivo = new StreamWriter("C:\\Programacion2\\empleados.csv");
+    for (int i = 0; i < pos; i++)
+    {
+        Console.WriteLine($"Registro {i+1}");
+        archivo.WriteLine($"{empleados[i].nombres}; {empleados[i].apellidos}; {empleados[i].cargo}; {empleados[i].salario}");
+        Console.WriteLine("Registro guardado.");
+    }
+    archivo.Close();
 }
 int menu()
 {
-    Console.Write("1. Agregar \n2. Mostrar \n3. Eliminar \n4. Salir \nDigite su opcion: ");
+    
+    Console.WriteLine("Menu:");
+    Console.Write("1. Agregar \n2. Mostrar \n3. Guardar \n4. Salir \nDigite su opcion: ");
     int op = int.Parse(Console.ReadLine()!);
     return op;
+}
+void mostrarDatos(int pos)
+{
+    Console.WriteLine("Mostrar registros");
+    Console.WriteLine("----------------------------");
+    for (int i=0; i<pos; i++)
+    {
+        Console.WriteLine($"Registro #{i+1}");
+        Console.WriteLine($"Nombre: {empleados[i].nombres} \nApellidos: {empleados[i].apellidos} " +
+            $" \nCargo: {empleados[i].cargo} \nSalario: {empleados[i].salario};");
+        Console.WriteLine("----------------------------");
+
+    }
+   
 }
 int main()
 {
     int op = 0, i = 0;
     do
     {
-        Console.WriteLine($"Registro {i+1}");
+        
         op = menu();
         switch (op)
         {
             case 1:
                 agregarEmpleado(i++);
-                break; 
-
-
+                break;
+            case 2:
+                Console.WriteLine("===================================");
+                mostrarDatos(i);
+                Console.WriteLine("===================================");
+                break;
+            case 3:
+                guardarEmpleados(i);
+                break;
+            default:
+                Console.WriteLine("Opcion invalida...");
+                break;
 
         }
     } while (op != 4);
