d7915b7bf1036b90790ef7322bcf46e9158dc722
miguel <magvegacruz@uamv.edu.ni>
2026-06-01 12:34:17 -0600

    agregue colorines y que pida presionar una tecla.

--- RegistroDeEmpleados/Program.cs
+++ RegistroDeEmpleados/Program.cs
@@ -13,6 +13,7 @@ void agregarEmpleado(int pos)
     empleados[pos].salario = double.Parse(Console.ReadLine()!);
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Registro guardado satisfactoriamente");
+    Console.ReadKey();
     Console.ResetColor();
     Console.WriteLine("===================================");
 }
@@ -24,14 +25,18 @@ void guardarEmpleados(int pos)
         Console.WriteLine($"Registro {i+1}");
         archivo.WriteLine($"{empleados[i].nombres}; {empleados[i].apellidos}; {empleados[i].cargo}; {empleados[i].salario}");
         Console.WriteLine("Registro guardado.");
+        Console.ReadKey();
     }
     archivo.Close();
 }
 int menu()
 {
-    
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Clear();
     Console.WriteLine("Menu:");
     Console.Write("1. Agregar \n2. Mostrar \n3. Guardar \n4. Salir \nDigite su opcion: ");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.ResetColor();
     int op = int.Parse(Console.ReadLine()!);
     return op;
 }
@@ -44,7 +49,9 @@ void mostrarDatos(int pos)
         Console.WriteLine($"Registro #{i+1}");
         Console.WriteLine($"Nombre: {empleados[i].nombres} \nApellidos: {empleados[i].apellidos} " +
             $" \nCargo: {empleados[i].cargo} \nSalario: {empleados[i].salario};");
+        Console.ReadKey();
         Console.WriteLine("----------------------------");
+       
 
     }
    
